baab94d74e8d9bc54162ef5153098c7871cfea55
Alex Lamb <alexlamb104@gmail.com>
2022-02-23 15:08:25 -0700

    Completed step 1, adding a movie to the ul

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 console.log('Javascript is successfully connected!')
+//global selectors
+let addBtn = document.querySelector('form')
 
 // Functions
+function addMovie(event){
+    event.preventDefault()
+    let inputField = document.querySelector('input');
+    let movie = document.createElement('li');
+    let movieTitle = document.createElement('span');
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle)
+    document.querySelector('ul').appendChild(movie)
+    inputField.value = ''
+}
+
+//Event Listeners
+addBtn.addEventListener('submit', addMovie)
