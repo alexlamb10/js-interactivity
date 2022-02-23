8faf0f1813f18ae70718c0e6ec5ce41c19d833b7
Alex Lamb <alexlamb104@gmail.com>
2022-02-23 15:15:51 -0700

    Completed step 3, add functioning delete button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-console.log('Javascript is successfully connected!')
+console.log('Javascript is successfully connected!');
 //global selectors
-let addBtn = document.querySelector('form')
+let addBtn = document.querySelector('form');
 
 // Functions
 function addMovie(event){
@@ -9,10 +9,17 @@ function addMovie(event){
     let movie = document.createElement('li');
     let movieTitle = document.createElement('span');
     movieTitle.textContent = inputField.value;
-    movie.appendChild(movieTitle)
-    document.querySelector('ul').appendChild(movie)
-    inputField.value = ''
+    movie.appendChild(movieTitle);
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    movie.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', deleteMovie)
+    document.querySelector('ul').appendChild(movie);
+    inputField.value = '';
 }
 
+function deleteMovie(event){
+    event.target.parentNode.remove()
+}
 //Event Listeners
-addBtn.addEventListener('submit', addMovie)
+addBtn.addEventListener('submit', addMovie);
